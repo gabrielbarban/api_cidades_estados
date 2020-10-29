600d0b7e856fcc4f649b9796db67918767e3a75d
GABRIEL BARBAN ROCHA <gbrocha@brasilseg.com.br>
2020-10-29 13:11:28 -0300

    add dump

---
+++ database/connection.php
@@ -0,0 +1,10 @@
+<?php
+	class Conexao
+	{
+	        public $host='localhost';
+	        public $port='3306';
+	        public $user='gabriel';
+	        public $password='gabriel';
+	        public $dbname="busca";
+	}
+?>
